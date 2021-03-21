b0887e5d75080321a3f4b4e9b5e7885d944a0adf
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-03-20 23:08:44 -0300

    Api 2.1

--- api/controllers/pokemon.js
+++ api/controllers/pokemon.js
@@ -1,13 +1,27 @@
+const pokemon = require("../routes/pokemon");
+
 module.exports = (app) => {
   const pokemon1 = app.data.pokemon;
   const listPokemon = app.data.pokemons;
   const controller = {};
 
-  controller.pokemon1 = (require, response) =>
+  controller.pokemon1 = (request, response) =>
     response.status(200).json(pokemon1);
 
-  controller.listPokemon = (require, response) =>
+  controller.listPokemonFilter = (request, response) => {
+    const filterPokemons = listPokemon.pokemons.filter((pokemon) => {
+      return pokemon.name === request.params.name;
+    });
+
+    const createObject = {
+      pokemons: filterPokemons,
+    };
+    response.status(200).json(createObject);
+  };
+
+  controller.listPokemon = (request, response) => {
     response.status(200).json(listPokemon);
+  };
 
   return controller;
 };
--- api/routes/pokemon.js
+++ api/routes/pokemon.js
@@ -2,5 +2,6 @@ module.exports = (app) => {
   const controller = app.controllers.pokemon;
 
   app.route("/api/v1/pokemon").get(controller.pokemon1);
-  app.route("/api/v1/pokemon/List").get(controller.listPokemon);
+  app.route("/api/v1/pokemon/list/:name").get(controller.listPokemonFilter);
+  app.route("/api/v1/pokemon/list").get(controller.listPokemon);
 };
